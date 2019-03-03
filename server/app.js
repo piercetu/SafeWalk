37e320b123d49c2109f57e04b574a8bf8d65beb3
Nhat Nguyen <31426677+nguyen-nhat@users.noreply.github.com>
2019-03-03 09:51:02 -0800

    Website

--- server/app.js
+++ server/app.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const path = require('path');
 
 const { PORT } = process.env;
 const routes = require('./routes');
 
 app.use(bodyParser.urlencoded({ extended: true }));
+app.get("/", (req, res) => {
+  return res.render(path.join(__dirname, 'public/index.html'));
+})
 app.use(routes);
+console.log('asdsadsadsadas')
 
 app.listen(PORT || 3000, () => console.log(`Server running on port ${PORT ? PORT : 3000}`));
 
