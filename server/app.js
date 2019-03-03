9a5855ac6d29f4b9703ee4ff576874ad8a0e54ca
Nhat Trinh <goodyshoes_nt@yahoo.com>
2019-03-02 22:44:22 -0800

    fixed gcloud errors

--- server/app.js
+++ server/app.js
@@ -8,6 +8,5 @@ const routes = require('./routes');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(routes);
 
-
 app.listen(PORT || 3000, () => console.log(`Server running on port ${PORT ? PORT : 3000}`));
 
