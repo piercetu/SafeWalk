180e5475da7f5c01c4c9d80966abf1fc01426ca5
Nhat Trinh <goodyshoes_nt@yahoo.com>
2019-03-02 18:35:11 -0800

    set up client server directory

---
+++ client/App.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import MapView from 'react-native-maps';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Hello</Text>
+        
+        {/* Intialize map */}
+        <MapView
+          style={styles.maps}
+          initialRegion={{
+            latitude: 37.78825,
+            longitude: -122.4324,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}
+        >
+
+        {/* Initialize marker */}
+          <MapView.Marker
+            coordinate={{
+              longitude: -122.4324, 
+              latitude: 37.78825
+            }}
+            title = {'My marker title'}
+            description = {'marker description'}
+          />
+          {/* End marker */}
+
+          {/* Intialize circle */}
+          <MapView.Circle 
+            center={{
+              latitude: 37.78825,
+              longitude: -122.4324,
+            }}
+            radius={240}
+            strokeWidth={2}
+            strokeColor="#3399ff"
+            fillColor="#80bfff"
+          />
+          {/* End circle */}
+        </MapView>
+        {/* End map  */}
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  maps: {
+    borderWidth: 2,
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0
+  }
+});
