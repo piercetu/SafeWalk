753ee87ef96fca584a278515828d23cfb7fb9e40
Nhat Nguyen <31426677+nguyen-nhat@users.noreply.github.com>
2019-03-03 06:50:45 -0800

    Working Map.js (Do npm install)

--- client/components/Map.js
+++ client/components/Map.js
@@ -1,20 +1,26 @@
 import React from 'react';
-import { StyleSheet, Text, View, TextInput } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 import MapView from 'react-native-maps';
+import geolib from 'geolib'
 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
 
-    console.log('>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\n\n\n')
+    console.log('>  Map Debug >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\n\n')
+
+    // 1) The parents inputs child destination
+    // 2) App grabs the GPS of child current location
+    // 3) 
 
     // Hard code - Should get the value from the home screen
-    // midpoint_lat = (latitude1 + latitude2) / 2;
-    // midpoint_long = (longitude1 + longitude2) / 2;
+    // midLat => (lat1 (lat of current) + lat2 (lat of destination)) / 2
+    // midLng => (lng1 + lng2) / 2
     const midpoint_lat = (37.78825 + 37.78200) / 2;
     const midpoint_long = (-122.4333 + -122.4333) / 2;
 
     this.state = {
+      radius: 740,
       longitude1: -122.4333,
       latitude1: 37.78825,
       longitude2: -122.4333,
@@ -23,20 +29,34 @@ export default class App extends React.Component {
       midpoint_lat: midpoint_lat,
     }
 
-    console.log('Midpoint Longitude:', (this.state.longitude2 + this.state.longitude2) / 2);
-    console.log('Midpoint Latitude:', (this.state.latitude1 + this.state.latitude2) / 2);
+    // Debugging
+    // console.log('Midpoint Longitude:', (this.state.longitude2 + this.state.longitude2) / 2);
+    // console.log('Midpoint Latitude:', (this.state.latitude1 + this.state.latitude2) / 2);
   }
 
+
   render() {
+
+    // Prints the distances away
+    getDistance = () => {
+      const coods = geolib.getDistanceSimple(
+        { latitude: this.state.latitude1, longitude: this.state.longitude1 },
+        { latitude: this.state.midpoint_lat, longitude: this.state.midpoint_long }
+      );
+
+      console.log('Debug:', coods);
+
+      // Checks whether inside or outside
+      const RADIUS = this.state.radius;
+      if (coods < RADIUS) {
+        console.log('INSIDE ✅')
+      } else {
+        console.log('OUTSIDE ❌')
+      }
+    }
+
     return (
       <View style={styles.container}>
-        {/* <Text>Hello</Text> */}
-
-        <TextInput style={styles.text}
-          style={{height: 40}}
-          placeholder = "Enter destination address."
-          onChangeText={(text) => this.setState({text})}
-        />
 
         {/* Intialize map */}
         {/* Current view of the map */}
@@ -60,13 +80,16 @@ export default class App extends React.Component {
               latitude: this.state.latitude1,
             }}
             title={'Child Marker'}
-            description={'marker description'}
-            // onPress={() => { console.log("LL:", this.state.latitude1, this.state.longitude1) }}
+            description={`lng:${this.state.longitude1} lat:${this.state.latitude1}`}
+            onPress={(e) => {
+              this.setState({ longitude1: e.nativeEvent.coordinate.longitude });
+              this.setState({ latitude1: e.nativeEvent.coordinate.latitude });
+              getDistance();
+            }}
             onDragEnd={(e) => {
               this.setState({ longitude1: e.nativeEvent.coordinate.longitude });
               this.setState({ latitude1: e.nativeEvent.coordinate.latitude });
-              console.log("LL:", this.state.latitude1, this.state.longitude1);
-              // console.log(e.nativeEvent.coordinate);
+              // getDistance();
             }}
           />
 
@@ -81,16 +104,26 @@ export default class App extends React.Component {
             description={'This marker does not move, the child maker should go here'}
           />
 
-          {/* Test Pointer */}
-          <MapView.Marker
+          {/* DEBUG MIDPOINT */}
+          {/* <MapView.Marker
+            coordinate={{
+              longitude: this.state.midpoint_long,
+              latitude: this.state.midpoint_lat,
+            }}
+            title={'MIDPOINT Marker'}
+            description={`lng:${this.state.midpoint_long} lat:${this.state.midpoint_lat}`}
+          /> */}
+
+          {/* DEBUG Test Pointer */}
+          {/* <MapView.Marker
             pinColor='#006eff'
             coordinate={{
               longitude: this.state.midpoint_long + 0.00845,
               latitude: this.state.midpoint_lat,
             }}
             title={'Desitation Marker'}
-            description={'This marker does not move, the child maker should go here'}
-          />
+            description={`lng:${this.state.longitude2} lat:${this.state.latitude2}`}
+          /> */}
 
           {/* Display circle of what the child has access to */}
           <MapView.Circle
@@ -98,7 +131,7 @@ export default class App extends React.Component {
               longitude: this.state.midpoint_long,
               latitude: this.state.midpoint_lat,
             }}
-            radius={740}
+            radius={this.state.radius}
             strokeWidth={2}
             strokeColor="#3399ff"
             fillColor="rgba(255,0,0,0.3)"
@@ -116,21 +149,14 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    // alignItems: 'center',
-    // justifyContent: 'center',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
-   maps: {
-    borderWidth: 2,
-    position: 'absolute', 
+  maps: {
+    position: 'absolute',
     top: 0,
     left: 0,
     right: 0,
-    bottom: 0,
-    zIndex: 1,
-  },
-  text: {
-    position: 'absolute',
-    zIndex: 2,
-    top: -5,
+    bottom: 0
   }
 });
