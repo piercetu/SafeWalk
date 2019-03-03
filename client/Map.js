c3b9d95d323854f9c25b8fba5df7f86fae5b7e5d
Nhat Nguyen <31426677+nguyen-nhat@users.noreply.github.com>
2019-03-03 00:30:30 -0800

    Updated Map: progress in circle

--- client/Map.js
+++ client/Map.js
@@ -2,130 +2,109 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import MapView from 'react-native-maps';
 
-export default class Map extends React.Component {
+export default class App extends React.Component {
   constructor(props) {
     super(props);
+
+    console.log('>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\n\n\n')
+
+    // Hard code - Should get the value from the home screen
+    // midpoint_lat = (latitude1 + latitude2) / 2;
+    // midpoint_long = (longitude1 + longitude2) / 2;
+    const midpoint_lat = (37.78825 + 37.78200) / 2;
+    const midpoint_long = (-122.4333 + -122.4333) / 2;
+
     this.state = {
-      latitude1: 37.78825,
       longitude1: -122.4333,
-      latitude2: 37.78200,
+      latitude1: 37.78825,
       longitude2: -122.4333,
+      latitude2: 37.78200,
+      midpoint_long: midpoint_long,
+      midpoint_lat: midpoint_lat,
     }
 
-    console.log('Latitude:', (this.state.latitude1 + this.state.latitude2) / 2);
-    console.log('Longitude:', (this.state.longitude2 + this.state.longitude2) / 2);
+    console.log('Midpoint Longitude:', (this.state.longitude2 + this.state.longitude2) / 2);
+    console.log('Midpoint Latitude:', (this.state.latitude1 + this.state.latitude2) / 2);
   }
 
   render() {
     return (
       <View style={styles.container}>
-        <Text>Hello</Text>
 
         {/* Intialize map */}
         {/* Current view of the map */}
         <MapView
           style={styles.maps}
           initialRegion={{
-            latitude: 37.78825,
             longitude: -122.4324,
-            latitudeDelta: 0.0922,
+            latitude: 37.78825,
             longitudeDelta: 0.0421,
+            latitudeDelta: 0.0922,
           }}
         >
 
-          {/* Initialize marker 1 */}
+          {/* Child Marker: This marker moves */}
           <MapView.Marker
-            coordinate={{
-              latitude: this.state.latitude1,
-              longitude: this.state.longitude1,
-            }}
-            title={'My marker title'}
-            description={'marker description'}
-          />
-          {/* End marker 1 */}
-
-          {/* Initialize marker 2 */}
-          <MapView.Marker
-            coordinate={{
-              latitude: this.state.latitude2,
-              longitude: this.state.longitude2,
-            }}
-            title={'My marker title'}
-            description={'marker description'}
-          />
-          {/* End marker 2 */}
-
-          {/* Midpoint */}
-          <MapView.Marker
-            coordinate={{
-              latitude: (this.state.latitude1 + this.state.latitude2) / 2,
-              longitude: (this.state.longitude2 + this.state.longitude2) / 2,
-            }}
-            title={'My marker title'}
-            description={'marker description'}
-          />
-          {/* Midpoint */}
-
-          {/* TEST */}
-          <MapView.Marker
-            pinColor='#48962b'
             draggable
+            pinColor='#ffff00'
+
             coordinate={{
-              latitude: ((this.state.latitude1 + this.state.latitude2) / 2),
-              longitude: ((this.state.longitude2 + this.state.longitude2) / 2) + 0.0078,
+              longitude: this.state.longitude1,
+              latitude: this.state.latitude1,
             }}
-            title={'Dragable'}
-            description={'This marker is Dragable'}
+            title={'Child Marker'}
+            description={'marker description'}
+            // onPress={() => { console.log("LL:", this.state.latitude1, this.state.longitude1) }}
+            onDragEnd={(e) => {
+              this.setState({ longitude1: e.nativeEvent.coordinate.longitude });
+              this.setState({ latitude1: e.nativeEvent.coordinate.latitude });
+              console.log("LL:", this.state.latitude1, this.state.longitude1);
+              // console.log(e.nativeEvent.coordinate);
+            }}
           />
-          {/* TEST */}
 
-          {/* Dragable */}
-          {/* <MapView initialRegion={{
-            latitude: ((this.state.latitude1 + this.state.latitude2) / 2),
-            longitude: ((this.state.longitude2 + this.state.longitude2) / 2) + 0.0078,
-            latitudeDelta: 0.0922,
-            longitudeDelta: 0.0421,
-          }}>
-            <Marker draggable
-              coordinate={this.state.x}
-              onDragEnd={(e) => this.setState({ x: e.nativeEvent.coordinate })}
-            />
-          </MapView> */}
+          {/* Desitation Marker: Static point */}
+          <MapView.Marker
+            pinColor='#006eff'
+            coordinate={{
+              longitude: this.state.longitude2,
+              latitude: this.state.latitude2,
+            }}
+            title={'Desitation Marker'}
+            description={'This marker does not move, the child maker should go here'}
+          />
 
-          {/* Intialize circle 1 */}
+          {/* Test Pointer */}
+          <MapView.Marker
+            pinColor='#006eff'
+            coordinate={{
+              longitude: this.state.midpoint_long + 0.00845,
+              latitude: this.state.midpoint_lat,
+            }}
+            title={'Desitation Marker'}
+            description={'This marker does not move, the child maker should go here'}
+          />
+
+          {/* Display circle of what the child has access to */}
           <MapView.Circle
             center={{
-              latitude: (this.state.latitude1 + this.state.latitude2) / 2,
-              longitude: (this.state.longitude1 + this.state.longitude2) / 2,
+              longitude: this.state.midpoint_long,
+              latitude: this.state.midpoint_lat,
             }}
             radius={740}
             strokeWidth={2}
             strokeColor="#3399ff"
             fillColor="rgba(255,0,0,0.3)"
           />
-          {/* End circle 1 */}
-
-          {/* Create polygon to cover walking distance */}
-          {/* <MapView.Polygon
-            coordinates={[
-              { latitude: this.state.latitude1 + 0.005, longitude: this.state.longitude1 + 0.005 },
-              { latitude: this.state.latitude1 - 0.005, longitude: this.state.longitude1 - 0.005 },
-              { latitude: this.state.latitude2 - 0.005, longitude: this.state.longitude2 - 0.005 },
-              { latitude: this.state.latitude2 + 0.005, longitude: this.state.longitude2 + 0.005 },
-            ]}
-            strokeWidth={2}
-            strokeColor="#3399ff"
-            fillColor="rgba(255,0,0,0.3)"
-          /> */}
-          {/* End polygon to cover walking distance */}
 
         </MapView>
-        {/* End map  */}
       </View>
     );
   }
 }
 
+
+// Compenents Styles
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -134,7 +113,6 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   maps: {
-    borderWidth: 2,
     position: 'absolute',
     top: 0,
     left: 0,
